3ad96020c7037cba89afd1a60107c031a0573ace
Vishwak Sena .C <chilukurvishwak21@gmail.com>
2025-07-24 21:04:02 +0000

    how should i do it?

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'live.staticflickr.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
