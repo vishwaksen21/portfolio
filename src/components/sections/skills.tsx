30afc34ea138b1abccf5ab84f0f2ea9a4de2a429
Vishwak Sena .C <chilukurvishwak21@gmail.com>
2025-07-24 20:13:01 +0000

    put logos based on the skill

--- src/components/sections/skills.tsx
+++ src/components/sections/skills.tsx
@@ -2,7 +2,7 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { motion } from 'framer-motion';
-import { Database, DatabaseZap, BrainCircuit, Code } from 'lucide-react';
+import { BrainCircuit } from 'lucide-react';
 import {
   Tooltip,
   TooltipContent,
@@ -15,30 +15,48 @@ const skills = [
   {
     name: 'Python',
     icon: (
-      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 3.5c1.1 0 2 .9 2 2v1h-4v-1c0-1.1.9-2 2-2zm1 13c-1.1 0-2-.9-2-2v-1h4v1c0 1.1-.9 2-2 2zm-4.46-7.54c.29-.29.68-.46 1.1-.46h4.72c.42 0 .81.17 1.1.46.58.58.58 1.54 0 2.12l-2.08 2.08c-.29.29-.68.46-1.1-.46H6.54c-.42 0-.81-.17-1.1-.46-.58-.58-.58-1.54 0-2.12l2.08-2.08zm8.92 0c.58-.58.58-1.54 0-2.12l-2.08-2.08c-.29-.29-.68-.46-1.1-.46H9.54c-.42 0-.81.17-1.1.46-.58.58-.58 1.54 0 2.12l2.08 2.08c.29.29.68.46 1.1.46h4.72c.42 0 .81-.17 1.1-.46z" />
+      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM8.5 7.5c1.1 0 2 .9 2 2v1H6.5v-1c0-1.1.9-2 2-2zm7 9c-1.1 0-2-.9-2-2v-1h4v1c0 1.1-.9 2-2 2zm-4.46-4.54c.29-.29.68-.46 1.1-.46h4.72c.42 0 .81.17 1.1.46.58.58.58 1.54 0 2.12l-2.08 2.08c-.29.29-.68.46-1.1.46H6.54c-.42 0-.81-.17-1.1-.46-.58-.58-.58-1.54 0-2.12l2.08-2.08zm4.46-5.42l2.08 2.08c.58.58.58 1.54 0 2.12-.29.29-.68.46-1.1.46H9.54c-.42 0-.81-.17-1.1-.46-.58-.58-.58-1.54 0-2.12l2.08-2.08c.29-.29.68-.46 1.1-.46h4.72c.42 0 .81.17 1.1.46z"/>
       </svg>
     ),
   },
   {
     name: 'C',
-    icon: <Code />,
+    icon: (
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+            <path d="M14.904 4.173c-2.313 1.12-3.899 3.52-3.899 6.223v3.208c0 2.704 1.586 5.103 3.899 6.223.772.374 1.895.16 2.4-.645.506-.806.23-1.874-.58-2.323-1.03-.49-1.724-1.51-1.724-2.65v-1.21c0-1.14.693-2.16 1.724-2.65.81-.448 1.087-1.517.58-2.323-.503-.805-1.627-1.02-2.4-.645zm-4.49 1.41C6.98 7.423 5.34 9.92 5.34 12.004c0 2.083 1.638 4.58 5.074 6.42.808.43 1.84.194 2.3-.62.46-.814.16-1.85-.66-2.29-2.22-1.2-3.71-2.69-3.71-4.04s1.49-2.84 3.71-4.04c.82-.44 1.12-1.476.66-2.29-.46-.814-1.492-1.05-2.3-.62z"/>
+        </svg>
+    ),
   },
   {
     name: 'Java',
     icon: (
-      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-0.01 15.5c-3.03 0-5.5-2.47-5.5-5.5 0-1.42.54-2.73 1.45-3.74l1.41 1.41C8.55 10.4 8.5 11.2 8.5 12c0 1.93 1.57 3.5 3.5 3.5s3.5-1.57 3.5-3.5c0-.8-.15-1.56-.4-2.24l1.41-1.41c.91 1.01 1.45 2.32 1.45 3.65 0 3.03-2.47 5.5-5.5 5.5z" />
+      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+          <path d="M12.016 8.87a3.492 3.492 0 0 0-3.483 3.496c0 .8.27 1.52.716 2.08l-1.43 1.432a5.485 5.485 0 0 1-1.192-3.512c0-3.03 2.454-5.483 5.484-5.483a5.485 5.485 0 0 1 5.04 3.496l-1.53.86a3.486 3.486 0 0 0-3.605-2.37zm5.55 4.572c-.895.492-1.594 1.344-1.9 2.37H8.344c.306-1.026 1-1.878 1.898-2.37h7.318zm-1.898 3.515h-3.52V22h-1.92v-5.043H6.628c-.46 0-.82-.36-.82-.818V9.177c0-.46.36-.82.82-.82h10.744c.46 0 .82.36.82.82v6.945c0 .46-.36.818-.82.818z"/>
       </svg>
     ),
   },
-  { name: 'SQL', icon: <Database /> },
-  { name: 'NoSQL', icon: <DatabaseZap /> },
+  { 
+    name: 'SQL', 
+    icon: (
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+            <path d="M20.25 7.5a.75.75 0 0 0-.75-.75H4.5a.75.75 0 0 0-.75.75v2.25a.75.75 0 0 0 .75.75h15a.75.75 0 0 0 .75-.75V7.5zM4.5 13.5h15a.75.75 0 0 0 .75-.75v-2.25a.75.75 0 0 0-.75-.75H4.5a.75.75 0 0 0-.75.75v2.25a.75.75 0 0 0 .75.75zM3.75 3A.75.75 0 0 0 3 3.75v16.5a.75.75 0 0 0 .75.75h16.5a.75.75 0 0 0 .75-.75V3.75a.75.75 0 0 0-.75-.75H3.75zM4.5 5.25h15a.75.75 0 0 1 0 1.5H4.5a.75.75 0 0 1 0-1.5zM4.5 18.75h15a.75.75 0 0 1 0 1.5H4.5a.75.75 0 0 1 0-1.5z"/>
+        </svg>
+    )
+  },
+  { 
+    name: 'NoSQL', 
+    icon: (
+        <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 15v-2h2v2h-2zm0-4v-2h2v2h-2zm0-4V7h2v2h-2z"/>
+        </svg>
+    )
+  },
   {
     name: 'Django',
     icon: (
-      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path d="M13.67 4.5H9.13L12 10.33l-2.87 5.83H12l5.5-11.16h-3.83M8.13 4.5H3.67L12 20.33h4.46L8.13 4.5z" />
+      <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+        <path d="M0 16.13V7.87L5.33 2h7.34l-3.67 7.47 3.67 7.47H7.34L3.67 13.8L0 16.13M24 .002v24H0V.002h24zM1.5 16.5v-9l4-6.93h6.63L8.8 8.04l3.33 6.8h-4.3L4.9 9.87l-2.07 3.57.9 1.56.67 1.5z"/>
       </svg>
     ),
   },
@@ -118,8 +136,8 @@ export function Skills() {
                     {skills.map((skill) => (
                       <Tooltip key={skill.name}>
                         <TooltipTrigger asChild>
-                           <div className="w-16 h-16 flex items-center justify-center bg-muted/50 rounded-lg transition-all duration-300 hover:bg-muted hover:scale-110 cursor-pointer">
-                              <div className="w-8 h-8 text-muted-foreground fill-current">
+                           <div className="w-16 h-16 flex items-center justify-center bg-muted/50 rounded-lg transition-all duration-300 hover:bg-muted hover:scale-110 cursor-pointer p-2">
+                              <div className="w-full h-full text-muted-foreground">
                                 {skill.icon}
                               </div>
                             </div>
